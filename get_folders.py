08297e62109a83a25e0c2958d5b676f7ee4a85c2
Shiva Sitaraman <kssrisingavenger@gmail.com>
2015-11-02 11:09:12 +0530

    Get Folder  names
    Returns names of subfolders inside the given folder.

---
+++ get_folders.py
@@ -0,0 +1,9 @@
+import sys,os
+if __name__=="__main__":
+    '''
+    Return foldewr names in given path to automatically create dictionary for
+    any input.
+    '''
+    for root,dirs,files in os.walk(sys.argv[1]):
+        if dirs!=[]:return [d.split('/')[-1] for d in dirs]
+    return []
